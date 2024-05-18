5b47966987c0591f9236135b647a8f72f28d48f8
Bahar Kose <110201916+baharkose@users.noreply.github.com>
2024-05-18 18:08:49 +0300

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -48,7 +48,7 @@ const {products, loading} = useProducts()
 
   return (
     // <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-    <div className="card-div">
+    <div className="card-div bg-blue-50">
       {/* <SearchInput search={search} setSearch={setSearch} />  //- ihtiyacımız kalmadı o nedenle yoruma aldık  */}
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
         All Products
